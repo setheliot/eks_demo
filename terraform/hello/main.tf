e33018e9b49e23d9db60461c09cf5177973517c3
Seth Eliot <seliot@arpio.io>
2025-03-07 20:07:16 -0800

    add hello world demo

---
+++ terraform/hello/main.tf
@@ -0,0 +1,19 @@
+terraform {
+  required_providers {
+    local = {
+      source = "hashicorp/local"
+    }
+  }
+}
+
+# Create a file with "Hello, World!" text
+resource "local_file" "hello_file" {
+  content  = "Hello, World!"
+  filename = "hello.txt"
+}
+
+
+output "filename" {
+  value = local_file.hello_file.filename 
+}
+
